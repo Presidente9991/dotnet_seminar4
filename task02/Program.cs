27c5c82fd03ea04a38f878768ee5211f1d52aeda
Alexander <alekszenin1999@gmail.com>
2023-06-21 21:24:57 +0300

    Написана программа по задаче 2

---
+++ task02/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{number} -> {SumAllDigits(number)}");
+
+int SumAllDigits(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
