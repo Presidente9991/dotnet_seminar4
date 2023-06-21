8c3633f5c2e52486d919db89b96099562f710901
Alexander <alekszenin1999@gmail.com>
2023-06-21 21:54:55 +0300

    Написана программа по задаче 3

---
+++ task03/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Write("Массив из 8 элементов: ");
+CreateEightArray();
+
+void CreateEightArray()
+{
+    int[] eightArray = new int[8];
+    for (int i = 0; i < eightArray.Length; i++)
+    {
+        eightArray[i] = new Random().Next(1, 100);
+        Console.Write(eightArray[i] + " ");
+    }
+}
